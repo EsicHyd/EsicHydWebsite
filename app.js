6b731f4faf3bf72feab341330a6471b62210718a
Paritosh <paritoshsrivastava9199@gmail.com>
2018-09-16 15:47:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views',__dirname+'/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response) {
+  response.render("index.ejs");
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node server is running at localhost:" + app.get('port'));
+})
